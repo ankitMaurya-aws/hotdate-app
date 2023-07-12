75bbb4c999019a18d5d24aa1715f15771a73fe11
AronWebSolutionPVTLTD <balbir01@lula.is>
2023-07-12 13:30:45 +0530

    landing page responsive

--- src/LandingPage/components/ImageWithText2.jsx
+++ src/LandingPage/components/ImageWithText2.jsx
@@ -6,21 +6,21 @@ const ImageWithText2 = () => {
         <section
         className="text-with-image  text-white py-6 md:py-8 lg:py-20 Hook__up-dating relative z-0
     before:absolute before:content-[''] lg:before:w-[502px] before:w-full before:h-[502px] before:left-0 lg:before:left-1/2
-    before:bg-no-repeat before:bg-cover before:bg-violet before:blur-[200px] before:rounded-502 before:bottom-[-125px] before:z-[-2] translateX(-50%)">
+    before:bg-no-repeat before:bg-cover before:bg-violet before:blur-[200px] before:rounded-502 before:bottom-[-125px] before:z-[-2] before:-translate-x-1/2 before:transform">
         <div className="container mx-auto">
-            <div className="grid grid-cols-1 lg:grid-cols-2 p-1 rounded-50px gradient">
+            <div className="grid grid-cols-1 lg:grid-cols-2 p-1 rounded-2xl sm:rounded-50px gradient">
                 <div className="right-block flex justify-center items-center">
                     <div className="right-block-img w-full h-full">
                         <img src="landingPage/images/medium-shot-romantic-couple-inside-1.png"
-                            className="w-full object-cover h-full rounded-t-46 rounded-l-46 rounded-bl-none rounded-br-none lg:rounded-bl-46 lg:rounded-tr-none lg:rounded-br-none" alt="partner" />
+                            className="w-full object-cover h-full rounded-t-2xl sm:rounded-t-46 sm:rounded-l-46 rounded-bl-none rounded-br-none lg:rounded-bl-46 lg:rounded-tr-none lg:rounded-br-none" alt="partner" />
                     </div>
                 </div>
                 <div
-                    className="left-block flex justify-center items-center Hookup-content rounded-r-46 lg:rounded-bl-none rounded-b-46 bg-2e2e2e">
-                    <div className="inner-block p-14 xl:p-16">
-                        <p className="text-xl md:text-xl xl:text-35px leading-45 font-light max-w-xs">Hook Up With Adult Dating at
+                    className="left-block flex justify-center items-center Hookup-content rounded-b-2xl roudned-r-none lg:rounded-r-46 lg:rounded-bl-none sm:rounded-b-46 bg-2e2e2e px-5">
+                    <div className="inner-block px-0 py-10 lg:p-14 xl:p-16">
+                        <p className="text-xl md:text-xl xl:text-35px sm:leading-45 font-light max-w-xs">Hook Up With Adult Dating at
                         </p>
-                        <h2 className="text-2xl md:text-4xl xl:text-50px leading-75 font-bold">Kaizen Globe®</h2>
+                        <h2 className="text-2xl md:text-4xl xl:text-50px sm:leading-75 sm:mb-0 mb-3 font-bold">Kaizen Globe®</h2>
                         <p className="text-sm md:text-base xl:text-lg mb-4 leading-29 max-w-420px">Adult dating through Adult Friend
                             Finder
                             saves you time and effort.
--- src/LandingPage/components/SignUp.jsx
+++ src/LandingPage/components/SignUp.jsx
@@ -4,29 +4,29 @@ const SignUp = () => {
     return (
         <section className='relative signup_section'>
             <div className='container mx-auto'>
-                <div className='p-[3px] gradient relative rounded-50px -mt-16 z-[99]'>
-                    <div className='p-2 bg-black rounded-50px'>
-                        <div className='grid grid-cols-2 bg-black rounded-50px items-center '>
-                            <div className='py-16 px-10 xl:px-20 text-white'>
-                                <p className='font-secondary_font text-5xl'>Sign Up Now!</p>
-                                <h3 className='text-35px font-bold'>Have some fun tonight!</h3>
+                <div className='p-[3px] gradient relative rounded-2xl sm:rounded-50px -mt-16 z-[99]'>
+                    <div className='p-2 bg-black rounded-2xl sm:rounded-50px'>
+                        <div className='grid lg:grid-cols-2 bg-black rounded-2xl sm:rounded-50px items-center '>
+                            <div className='py-8 px-3 sm:py-16 sm:px-10 xl:px-20 text-white'>
+                                <p className='font-secondary_font text-3xl sm:text-5xl'>Sign Up Now!</p>
+                                <h3 className='text-2xl sm:text-35px font-bold'>Have some fun tonight!</h3>
                                 <div className='grid grid-cols-2 max-w-md mt-5'>
                                     <div>
                                         <span className='block max-w-[42px] h-1 bg-white mb-1'></span>
-                                        <p className='text-3xl font-bold'>98,897,765</p>
-                                        <p className='text-lg font-normal'>HOT PHOTOS</p>
+                                        <p className='text-lg sm:text-3xl font-bold'>98,897,765</p>
+                                        <p className='text-sm sm:text-lg font-normal'>HOT PHOTOS</p>
                                     </div>
                                     <div>
                                         <span className='block max-w-[42px] h-1 bg-white mb-1'></span>
-                                        <p className='text-3xl font-bold'>3,594,914</p>
-                                        <p className='text-lg font-normal'>CONNECTIONS</p>
+                                        <p className='text-lg sm:text-3xl font-bold'>3,594,914</p>
+                                        <p className='text-sm sm:text-lg font-normal'>CONNECTIONS</p>
                                     </div>
                                 </div>
                             </div>
-                            <div className='bg-white p-10 rounded-50px h-full'>
-                                <h3 className='text-5xl text-center font-secondary_font'>Find Your Date</h3>
+                            <div className='bg-white p-5 sm:p-10 rounded-2xl sm:rounded-50px h-full'>
+                                <h3 className='text-3xl sm:text-5xl text-center font-secondary_font'>Find Your Date</h3>
                                 <form>
-                                    <div className='grid grid-cols-2 gap-x-14 gap-y-2 mt-8'>
+                                    <div className='grid sm:grid-cols-2 gap-x-14 gap-y-2 mt-8'>
                                         <div>
                                             <label className='block text-lg'>I am a</label>
                                             <div className='p-[1px] gradient rounded-xl'>
